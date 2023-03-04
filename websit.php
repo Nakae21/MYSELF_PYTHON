46a9ab42c81b3fd566eb33c2abd7b43c6524b7fb
Nakae21 <100693631+Nakae21@users.noreply.github.com>
2023-03-04 18:56:03 +0000

    Create websit.php

---
+++ websit.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Welcome to NakatTekq IT Solutions</title>
+  </head>
+  <body>
+    <h1>Welcome to NakatTekq IT Solutions</h1>
+    <p>Subscribe to my <?php echo '<a href="https://www.youtube.com/c/NAKAEtekq">YouTube channel</a>'; ?>!</p>
+  </body>
+</html>
